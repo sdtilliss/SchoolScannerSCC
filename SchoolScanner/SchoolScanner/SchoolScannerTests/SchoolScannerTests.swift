7a67f801439d6b4d7b2913f10c63554a552ed4e6
Seth Tilliss <sdtilliss@gmail.com>
2019-05-25 17:48:25 -0400

    init

---
+++ SchoolScanner/SchoolScanner/SchoolScannerTests/SchoolScannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  SchoolScannerTests.swift
+//  SchoolScannerTests
+//
+//  Created by Seth Tilliss on 5/22/19.
+//  Copyright © 2019 Seth Tilliss. All rights reserved.
+//
+
+import XCTest
+@testable import SchoolScanner
+
+class SchoolScannerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
